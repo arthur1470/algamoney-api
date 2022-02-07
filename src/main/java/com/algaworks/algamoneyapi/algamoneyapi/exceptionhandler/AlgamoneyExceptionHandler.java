bb65c590446b01dc8a81da10fa427defd029c7fb
Arthur Marques <arthur.l2@hotmail.com>
2022-02-07 17:42:01 -0300

    DeleteMapping added for Person Resource

--- src/main/java/com/algaworks/algamoneyapi/algamoneyapi/exceptionhandler/AlgamoneyExceptionHandler.java
+++ src/main/java/com/algaworks/algamoneyapi/algamoneyapi/exceptionhandler/AlgamoneyExceptionHandler.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +16,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -42,6 +45,18 @@ public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler {
 		
 		List<Error> error = getErrorList(ex.getBindingResult());
 		return handleExceptionInternal(ex, error, headers, HttpStatus.BAD_REQUEST, request);
+	}
+	
+	@ExceptionHandler({ EmptyResultDataAccessException.class })
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public ResponseEntity<Object> handleEmptyResultDataAccessException(WebRequest request, EmptyResultDataAccessException ex) {
+		
+		String userErrorMessage = messageSource.getMessage("resource.not-found", null, LocaleContextHolder.getLocale());
+		String developerErrorMessage = ex.toString();
+		
+		List<Error> error = Arrays.asList(new Error(userErrorMessage, developerErrorMessage));
+		
+		return handleExceptionInternal(ex, error, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
 	}
 	
 	private List<Error> getErrorList(BindingResult bindingResult) {
--- src/main/java/com/algaworks/algamoneyapi/algamoneyapi/resource/PersonResource.java
+++ src/main/java/com/algaworks/algamoneyapi/algamoneyapi/resource/PersonResource.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -55,5 +56,10 @@ public class PersonResource {
 		}
 		
 		return ResponseEntity.ok(person.get());
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deletePerson(@PathVariable String id) {
+		personRepository.deleteById(id);		
 	}
 }
